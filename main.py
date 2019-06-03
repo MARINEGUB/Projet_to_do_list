df1a3e678a76543b225832632ecc9f2ed712188e
Marine Gubler <marine.gubler@gmail.com>
2019-06-03 16:17:00 +0100

    rajouter la fonctionnalité de suppression

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 class App:
   def __init__(self, to_do_list):
     self.to_do_list = to_do_list
-    
+    self.running = False
+
   def start(self):
-    while True:
+    self.running = True
+    while self.running:
       answer = input("Voulez-vous ajouter un item? --> Ecrivez OUI ou NON: ")
       answer = answer.upper()
 
@@ -11,16 +13,30 @@ class App:
         a = input("Que voulez-vous rajouter d'autre ?")
         self.to_do_list.insert(-1,a)
         print("Voici votre to-do list maj:")
-        [print(i) for i in self.to_do_list] 
+        self.print()
    
       elif answer == "NON":
-        print("Vous avez terminé votre to-do list")
-        print("Voici votre to-do list:")
-        [print(i) for i in self.to_do_list] 
-        break
+        answer = input("Voulez-vous supprimer un item? --> Ecrivez OUI ou NON: ")
+        answer = answer.upper()
+        if answer == "OUI":
+          a = input ("Quel item voulez-vous supprimer ?")
+          self.to_do_list.remove(a)
+          print("Voici votre to-do list maj:")
+          self.print()
+        elif answer == "NON":
+          print("Vous avez terminé votre to-do list")
+          print("Voici votre to-do list:")
+          self.print()
+          self.running = False
+        else:
+          print("Ceci n'est pas une réponse par OUI ou par NON") 
 
       else:
         print("Ceci n'est pas une réponse par OUI ou par NON")
+
+  def print(self):
+    [print(i) for i in self.to_do_list] 
+
 
 app = App(["manger", "boire", "dormir"])
 
